d3c77ada54d05441f92aa68ada51f2f87302f688
mathilderenversade <mathirenversade@gmail.com>
2020-11-02 11:00:25 +0100

    add contact form and some style on it

--- exemple1/contact.php
+++ exemple1/contact.php
@@ -3,21 +3,28 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Contactez-nous</title>
+    <title>Contact</title>
+    <link rel="stylesheet" href="reset.css">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="reset.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 <?php include 'header.php'; ?>
-    <h1>Contactez-nous</h1>
-    <p>Envoyez-nous vos coordonnées pour rentrer en contact avec nous</p>
-    <form>
-        <label>Entrez votre nom</label>
-        <input type='text'>
-        <label>Entrez votre prénom</label>
-        <input type='text'>
-        <label>Entrez votre email</label>
-        <input type='email'>
-        <input type="submit" value="Envoyer">
-    </form>
+ 
+    <h1 class="title-center">Contactez-nous</h1>
+    <p class="title-center">Envoyez-nous vos coordonnées pour rentrer en contact avec nous</p>
+    <div class="center">
+        <form>
+            <label>Entrez votre nom</label>
+            <input type='text'name='nom'>
+            <label>Entrez votre prénom</label>
+            <input type='text' name='prenom'>
+            <label>Entrez votre email</label>
+            <input type='email' name='email'>
+            <input type="submit" value="Envoyer">
+        </form>
+    </div>
     <?php include 'footer.php'; ?>
 </body>
 </html>
--- exemple1/footer.php
+++ exemple1/footer.php
@@ -1,12 +1,12 @@
 <?php 
 
 echo '
-<footer>
-    <ul>
-        <li> <a class="link" href="presentation.php">Mentions légales</a></li>
-        <li> <a class="link" href="contact.php">Contact</a></li>
-        <li> <a class="link" href="#">Notre revue de presse</a></li>
-    </ul>
-</footer>';
+    <footer>
+        <ul>
+            <li> <a class="link" href="presentation.php">Mentions légales</a></li>
+            <li> <a class="link" href="contact.php">Contact</a></li>
+            <li> <a class="link" href="#">Notre revue de presse</a></li>
+        </ul>
+    </footer>';
 
 ?>
