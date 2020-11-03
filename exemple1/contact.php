0f8b4259f1795e5688e8542df80b6834216d8697
mathilderenversade <mathirenversade@gmail.com>
2020-11-03 10:56:23 +0100

    add comments to explain

--- exemple1/contact.php
+++ exemple1/contact.php
@@ -25,13 +25,23 @@ if (!empty($_GET)){
     <h1 class="title-center">Contactez-nous</h1>
     <p class="title-center">Envoyez-nous vos coordonnées pour rentrer en contact avec nous</p>
     <div class="center">
+
+        <!-- Pour envoyer son formulaire 2 attributs sont nécessaires :
+        * action contient l'adresse de la page où les informations du formulaire seront envoyées. Si vide c'est la page courante
+        * method défini la méthode utilisée pour envoyer ces données, c'est soit en "get", soit en "post"
+        -->
+        
         <form action="" method="get">
             <label>Entrez votre nom</label>
-            <input type='text'name='nom'>
+            <!-- les champs souhaitants être utilisés pour transmettre des informations doivent posséder l'attribut name qui associe un nom à la future valeur. Concrètement ce nom sera la clé dans le array de la super global $_GET (methode en "get) ou bien $_POST (methode en "post").
+            En savoir plus les super globales php : https://www.php.net/manual/fr/language.variables.superglobals
+             -->
+             <!-- l'attribut placeholder permet d'afficher à l'intérieur du champ vide, une information à l'utilisateur sur la valeur attendue -->
+            <input type='text'name='nom' placeholder="entrez votre nom svp">
             <label>Entrez votre prénom</label>
             <input type='text' name='prenom' minlength="2" maxlength="12">
             <label>Entrez votre email</label>
-            <input type='email' name='email'>
+            <input type='email' name='email' placeholder="on veut votre email ici">
             
             <!--
             <div class="center">
@@ -44,7 +54,8 @@ if (!empty($_GET)){
             On peut rajouter des attributs à ces différents inputs exemple type, exemple minlenght, maxlenght.
             Il faut les tester pour les découvrir
             
-            Dans le cas de l'input type="radio" le fonctionnement est similaire à celui de type="checkbox", la différence majeure est que dans un type="radio" le choix est exclusive -->
+            Dans le cas de l'input type="radio" le fonctionnement est similaire à celui de type="checkbox", la différence majeure est que dans un type="radio" le choix est exclusive 
+            Dans le cas d'input de type="checkbox" et "radio", l'attribut checked permet de sélectionner par défaut une option -->
             <!--
             <div class="center">
                 <label for="choiceUn">Je suis client</label>
@@ -56,6 +67,9 @@ if (!empty($_GET)){
             </div>
             -->
             <input type="submit" value="Envoyer">
+            <!-- ou bien :
+            <button>Envoyer</button>
+             -->
         </form>
     </div>
     <?php include 'footer.php'; ?>
